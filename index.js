dbf56ad570d10c4fcd08664c2d091d5d3169a525
JrDev06 <jrbusaco72@gmail.com>
2024-09-22 10:25:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const { spawn } = require("child_process");
 const express = require("express");
 const path = require("path");
-const cron = require('node-cron');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,10 +14,6 @@ app.listen(port, '0.0.0.0', () => {
 });
 
 let botProcess = null;
-
-const getRandomRestartHours = () => {
-    return Math.floor(Math.random() * (9 - 7 + 1)) + 7;
-};
 
 const manageBotProcess = (script) => {
     if (botProcess) {
@@ -34,10 +29,6 @@ const manageBotProcess = (script) => {
 
     botProcess.on("close", (exitCode) => {
         console.log(`${script} terminated with code: ${exitCode}`);
-        if (exitCode !== 0) {
-            console.log(`Restarting ${script} in 3 seconds...`);
-            setTimeout(() => manageBotProcess(script), 3000);
-        }
     });
 
     botProcess.on("error", (error) => {
@@ -46,14 +37,3 @@ const manageBotProcess = (script) => {
 };
 
 manageBotProcess("ryuu.js");
-
-cron.schedule('0 * * * *', () => {
-    const hours = getRandomRestartHours();
-    console.log(`Scheduled bot restart in ${hours} hours.`);
-
-    const restartCron = cron.schedule(`0 */${hours} * * *`, () => {
-        console.log('Restarting bot process due to scheduled interval.');
-        manageBotProcess("ryuu.js");
-        restartCron.stop();
-    });
-});
